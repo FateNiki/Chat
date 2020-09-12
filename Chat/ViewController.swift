a64d5089438e27920b2454bffdaae4a7835a06a2
Aleksey Nikitin <fateniki@gmail.com>
2020-09-12 22:59:03 +0400

    Init Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Алексей Никитин on 12.09.2020.
+//  Copyright © 2020 Алексей Никитин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
