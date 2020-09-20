4313d2aca93b73b6c2e4bd417aa5a42924dc1bf3
Aleksey Nikitin <fateniki@gmail.com>
2020-09-20 19:52:14 +0400

    Добавил кнопку сохранить. Настройка кнопки сохранить через ViewController. Фикс constraint'ов лейблов

--- Chat/ViewController.swift
+++ Chat/ViewController.swift
@@ -9,21 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // MARK: Outlets
+    // MARK: Variables
+    private let lightGray = UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1)
+    
+    // MARK: - Outlets
     @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var saveButton: UIButton!
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configAvatarImageView()
+        configSaveButton()
     }
     
     // MARK: - Interface configuring
     private func configAvatarImageView() -> Void {
         let minSize = min(avatarImageView.layer.frame.width, avatarImageView.layer.frame.height)
         avatarImageView.layer.cornerRadius = minSize / 2
-        avatarImageView.backgroundColor = .lightGray
+        avatarImageView.backgroundColor = lightGray
+    }
+    
+    private func configSaveButton() -> Void {
+        saveButton.backgroundColor = lightGray
+        saveButton.layer.cornerRadius = 14
     }
 
 }
