0c6551bcc185ac6fb0a194808296012269def724
Aleksey Nikitin <fateniki@gmail.com>
2020-09-21 13:05:10 +0400

    Выбор изображения для аватара через галерею и камеру

--- Chat/ViewController.swift
+++ Chat/ViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // MARK: Variables
+    // MARK: - Interface constants
     private let lightGray = UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1)
+    private let saveButtonCornerRadius: CGFloat = 14
+    
+    // MARK: - Variables
     public var currentUser: User = mockUser
+    fileprivate var imagePicker: UIImagePickerController!
+
     
     // MARK: - Outlets
     @IBOutlet weak var avatarImageView: UIImageView!
@@ -38,7 +43,7 @@ class ViewController: UIViewController {
     
     private func configSaveButton() -> Void {
         saveButton.backgroundColor = lightGray
-        saveButton.layer.cornerRadius = 14
+        saveButton.layer.cornerRadius = saveButtonCornerRadius
     }
     
     private func initUserFields() -> Void {
@@ -53,24 +58,57 @@ class ViewController: UIViewController {
     @IBAction func editAvatarButtonDidTap(_ sender: UIButton) {
         let editAvatarDialog = UIAlertController(title: "Выбрать аватар", message: nil, preferredStyle: .actionSheet)
         
-        let galaryAction = UIAlertAction(title: "Установить из галлереи", style: .default) { (action:UIAlertAction) in
-            print("Установить из галлереи");
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "Установить из галлереи", style: .default) { _ in
+                self.selectImageFrom(.photoLibrary)
+            }
+            editAvatarDialog.addAction(photoLibraryAction)
         }
-
-        let takePhotoAction = UIAlertAction(title: "Сделать фото", style: .default) { (action:UIAlertAction) in
-            print("Сделать фото");
-        }
-
-        let cancelAction = UIAlertAction(title: "Отменить", style: .cancel)
         
-        editAvatarDialog.addAction(galaryAction)
-        editAvatarDialog.addAction(takePhotoAction)
-        editAvatarDialog.addAction(cancelAction)
+        if UIImagePickerController.isSourceTypeAvailable(.camera)  {
+            let takePhotoAction = UIAlertAction(title: "Сделать фото", style: .default) { _ in
+                self.selectImageFrom(.camera)
+            }
+            editAvatarDialog.addAction(takePhotoAction)
+        }
+
+        guard editAvatarDialog.actions.count > 0 else {
+            return
+        }
+        
+        editAvatarDialog.addAction(UIAlertAction(title: "Отменить", style: .cancel))
         present(editAvatarDialog, animated: true)        
     }
-    
-    // MARK: - Change avatar methods
-    
+}
 
+// MARK: - Work with ImagePicker
+extension ViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    enum ImageSource {
+        case photoLibrary
+        case camera
+    }
+    
+    
+    func selectImageFrom(_ source: ImageSource){
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        switch source {
+        case .camera:
+            imagePicker.sourceType = .camera
+        case .photoLibrary:
+            imagePicker.sourceType = .photoLibrary
+        }
+        present(imagePicker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        imagePicker.dismiss(animated: true, completion: nil)
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            print("Image not found!")
+            return
+        }
+        currentUser.avatar = selectedImage.pngData()
+        initUserFields()
+    }
 }
 
