5cbbc80242e9691347b09353454040fc8ff08209
Aleksey Nikitin <fateniki@gmail.com>
2020-09-26 21:10:50 +0400

    Добавил кастомную ячейку для таблицы ConversationsList.

---
+++ Chat/Views/ConversationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ConversationTableViewCell.swift
+//  Chat
+//
+//  Created by Алексей Никитин on 26.09.2020.
+//  Copyright © 2020 Алексей Никитин. All rights reserved.
+//
+
+import UIKit
+
+class ConversationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
