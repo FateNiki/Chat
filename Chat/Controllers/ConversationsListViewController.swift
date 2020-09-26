3028e37632ef1cb4c7b052c6fcbbed7095ab320b
Aleksey Nikitin <fateniki@gmail.com>
2020-09-26 22:00:28 +0400

    Сделал отображение ячеек по умолчанию

--- Chat/Controllers/ConversationsListViewController.swift
+++ Chat/Controllers/ConversationsListViewController.swift
@@ -9,8 +9,15 @@
 import UIKit
 
 class ConversationsListViewController: UIViewController {
+    // MARK: - Constants
+    private let conversationCellIdentifier = String(describing: ConversationTableViewCell.self)
+    
     private lazy var tableView: UITableView = {
-        let tableView = UITableView(frame: view.frame)
+        let tableView = UITableView(frame: view.frame, style: .plain)
+        tableView.register(UINib(nibName: conversationCellIdentifier, bundle: nil), forCellReuseIdentifier: conversationCellIdentifier)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
         return tableView
     }()
 
@@ -25,4 +32,23 @@ class ConversationsListViewController: UIViewController {
         navigationItem.title = "Tinkoff Chat"
     }
 
+}
+
+extension ConversationsListViewController: UITableViewDelegate {    
+}
+
+extension ConversationsListViewController: UITableViewDataSource {
+    // Return the number of rows for the table.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return 10
+    }
+
+    // Provide a cell object for each row.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       // Fetch a cell of the appropriate type.
+        let cell: ConversationTableViewCell = tableView.dequeueReusableCell(withIdentifier: conversationCellIdentifier, for: indexPath) as! ConversationTableViewCell
+
+       return cell
+    }
+    
 }
