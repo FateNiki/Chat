732538df837857b9bdef216e581e083212111a42
Aleksey Nikitin <fateniki@gmail.com>
2020-09-26 20:36:17 +0400

    Рефактор структуры проекта. Создал папку для контроллеров

---
+++ Chat/Controllers/ConversationsListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ConversationsListViewController.swift
+//  Chat
+//
+//  Created by Алексей Никитин on 26.09.2020.
+//  Copyright © 2020 Алексей Никитин. All rights reserved.
+//
+
+import UIKit
+
+class ConversationsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
