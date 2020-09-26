869a4f6548323a353b4728c0fed2547720efb431
Aleksey Nikitin <fateniki@gmail.com>
2020-09-26 23:03:09 +0400

    Добавил модели сообщения и диалога

---
+++ Chat/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Chat
+//
+//  Created by Алексей Никитин on 26.09.2020.
+//  Copyright © 2020 Алексей Никитин. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let text: String
+    let date: Date
+    var isRead: Bool
+}
