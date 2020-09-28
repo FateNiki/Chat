538e3daf2dfa87e28a9a26cbdfce3b1ff0d38bc2
Aleksey Nikitin <fateniki@gmail.com>
2020-09-28 13:54:53 +0400

    Добавил классы 2х ячеек для сообщений

---
+++ Chat/Views/IncomeMessageTableViewCell/IncomeMessageTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  IncomeMessageTableViewCell.swift
+//  Chat
+//
+//  Created by Алексей Никитин on 28.09.2020.
+//  Copyright © 2020 Алексей Никитин. All rights reserved.
+//
+
+import UIKit
+
+class IncomeMessageTableViewCell: UITableViewCell {
+    
+}
