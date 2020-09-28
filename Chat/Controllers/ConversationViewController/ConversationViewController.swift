45a3c2c078ad7da78e6a924fbaae7c5763331766
Aleksey Nikitin <fateniki@gmail.com>
2020-09-28 14:21:58 +0400

    Зарегистрировал ячейки сообщений, отображение тестовых данных

--- Chat/Controllers/ConversationViewController/ConversationViewController.swift
+++ Chat/Controllers/ConversationViewController/ConversationViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ConversationViewController: UIViewController {
+    // MARK: - Constants
+    private let incomeMessageCellIdentifier = String(describing: IncomeMessageTableViewCell.self)
+    private let outcomeMessageCellIdentifier = String(describing: OutcomeMessageTableViewCell.self)
+
     // MARK: - Variables
     var conversation: Conversation?
+    var messages = messagesMock.shuffled()
     
     // MARK: - UI Variables
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: view.frame, style: .plain)
+        tableView.register(UINib(nibName: incomeMessageCellIdentifier, bundle: nil), forCellReuseIdentifier: incomeMessageCellIdentifier)
+        tableView.register(UINib(nibName: outcomeMessageCellIdentifier, bundle: nil), forCellReuseIdentifier: outcomeMessageCellIdentifier)
         tableView.dataSource = self
         return tableView
     }()
@@ -43,14 +50,26 @@ class ConversationViewController: UIViewController {
 
 
 extension ConversationViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { 1 }
+    func numberOfSections(in tableView: UITableView) -> Int { 1 }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { messages.count }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        var cell: UITableViewCell
+        if indexPath.row % 2 == 0 {
+            cell = tableView.dequeueReusableCell(withIdentifier: incomeMessageCellIdentifier) ?? UITableViewCell()
+            if let messageCell = cell as? IncomeMessageTableViewCell {
+                messageCell.configure(with: .init(message: messages[indexPath.row].text))
+            }
+        } else {
+            cell = tableView.dequeueReusableCell(withIdentifier: outcomeMessageCellIdentifier) ?? UITableViewCell()
+            if let messageCell = cell as? OutcomeMessageTableViewCell {
+                messageCell.configure(with: .init(message: messages[indexPath.row].text))
+            }
+        }
+        
         return cell
     }
-    
-    func numberOfSections(in tableView: UITableView) -> Int { 1 }
     
         
 }
