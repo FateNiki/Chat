ddf920f5e6c8c0414de4a3e45c100a8bf22b8078
Aleksey Nikitin <fateniki@gmail.com>
2020-10-05 15:12:59 +0400

    View для отображение примера цветовой схемы

---
+++ Chat/Views/ThemePlaceholderView/ThemePlaceholderView.swift
@@ -0,0 +1,89 @@
+//
+//  ThemePlaceholderView.swift
+//  Chat
+//
+//  Created by Алексей Никитин on 05.10.2020.
+//  Copyright © 2020 Алексей Никитин. All rights reserved.
+//
+
+import UIKit
+
+class ThemePlaceholderView: UIView {
+    // MARK: - Interface variables
+    static private let activeBorderColor = UIColor(red: 0.00, green: 0.48, blue: 1.00, alpha: 1.00).cgColor
+    static private let inactiveBorderColor = UIColor(red: 0.59, green: 0.59, blue: 0.59, alpha: 1.00).cgColor
+    
+    // MARK: - Outlets
+    @IBOutlet weak var incomePlaceholderLabel: PaddingLabel!
+    @IBOutlet weak var outcomePlaceholderLabel: PaddingLabel!
+    
+    // MARK: - Variables
+    private(set) var isActive: Bool = false
+    private(set) var theme: Theme? {
+        didSet {
+            updateView()
+        }
+    }
+
+    
+    // MARK: - Create from xib
+    private func initViewFromXib() -> UIView? {
+        let xibName = String(describing: type(of: self))
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: xibName, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        updateView()
+    }
+    
+    
+    // MARK: - Interface configuring
+    private func setupView() {
+        guard let xibView = initViewFromXib() else { return }
+        xibView.frame = self.bounds
+        xibView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.addSubview(xibView)
+        
+        self.layer.cornerRadius = 14
+        self.clipsToBounds = true
+        
+        incomePlaceholderLabel.layer.cornerRadius = 10
+        incomePlaceholderLabel.clipsToBounds = true
+        outcomePlaceholderLabel.layer.cornerRadius = 10
+        outcomePlaceholderLabel.clipsToBounds = true
+    }
+    
+    private func updateView() -> Void {
+        guard let theme = theme else { return }
+        
+        self.backgroundColor = theme.backgroundColor
+        self.layer.borderWidth = isActive ? 3 : 1
+        self.layer.borderColor = isActive ? Self.activeBorderColor : Self.inactiveBorderColor
+        
+        
+        incomePlaceholderLabel.backgroundColor = theme.incomeMessageCellColor
+        incomePlaceholderLabel.textColor = theme.incomeMessageTextColor
+        outcomePlaceholderLabel.backgroundColor = theme.outcomeMessageCellColor
+        outcomePlaceholderLabel.textColor = theme.outcomeMessageTextColor
+    }
+}
+
+extension ThemePlaceholderView: ConfigurableView {
+    func configure(with model: Theme) {
+        theme = model
+    }
+}
