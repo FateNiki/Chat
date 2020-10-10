43dad89baee5b3d4b0dfa74940e3305e37bf94f7
Aleksey Nikitin <fateniki@gmail.com>
2020-10-11 00:02:30 +0400

    Тестирование медленной загрузки с использованием DispatchQueue

--- Chat/ViewModel/UserManager.swift
+++ Chat/ViewModel/UserManager.swift
@@ -21,13 +21,17 @@ class GCDUserManager: UserManager {
     static let shared = GCDUserManager()
     
     func saveToFile(data: UserData, completion: @escaping (User) -> Void) {
-        UserMock.currentUser.avatar = data.avatar
-        UserMock.currentUser.description = data.description
-        let names = data.fullName.split(separator: Character(" "), maxSplits: 2, omittingEmptySubsequences: true)
-        UserMock.currentUser.firstName = names.count > 0 ? String(names[0]) : ""
-        UserMock.currentUser.lastName = names.count > 1 ? String(names[1]) : ""
 
-        completion(UserMock.currentUser)
+        DispatchQueue.global().async {
+            sleep(5)
+            UserMock.currentUser.avatar = data.avatar
+            UserMock.currentUser.description = data.description
+            let names = data.fullName.split(separator: Character(" "), maxSplits: 2, omittingEmptySubsequences: true)
+            UserMock.currentUser.firstName = names.count > 0 ? String(names[0]) : ""
+            UserMock.currentUser.lastName = names.count > 1 ? String(names[1]) : ""
+
+            completion(UserMock.currentUser)
+        }        
     }
     
     func loadFromFile(completion: @escaping (User) -> Void) {
