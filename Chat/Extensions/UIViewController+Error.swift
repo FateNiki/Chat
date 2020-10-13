0025215324c38205054544545bbc8f845f97ee25
Aleksey Nikitin <fateniki@gmail.com>
2020-10-13 18:42:29 +0400

    Дополнительный метод открытия модалки с alert

--- Chat/Extensions/UIViewController+Error.swift
+++ Chat/Extensions/UIViewController+Error.swift
@@ -14,5 +14,13 @@ extension UIViewController {
         errorAlertController.addAction(UIAlertAction(title: "Закрыть", style: .cancel))
         present(errorAlertController, animated: true)
     }
+    
+    func openErrorAlert(title: String, message: String, buttons: [UIAlertAction]) {
+        let errorAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        for button in buttons {
+            errorAlertController.addAction(button)
+        }        
+        present(errorAlertController, animated: true)
+    }
 }
 
