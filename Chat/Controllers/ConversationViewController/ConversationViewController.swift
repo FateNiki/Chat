5f19128b9d9e25b6b9c28ea5ea5b2fd3dcd1fc88
Aleksey Nikitin <fateniki@gmail.com>
2020-10-18 19:02:14 +0400

    Список сообщений роботает на FirebaseDS

--- Chat/Controllers/ConversationViewController/ConversationViewController.swift
+++ Chat/Controllers/ConversationViewController/ConversationViewController.swift
@@ -12,15 +12,18 @@ import Firebase
 class ConversationViewController: UIViewController {
     // MARK: - Constants
     private let messageCellIdentifier = String(describing: MessageTableViewCell.self)
-    private lazy var messagesReference: CollectionReference = {
+    private var messageDataSource: FirebaseDataSource<Message>!
+    private lazy var messagesQuery: Query = {
         let db = Firestore.firestore()
-        return db.collection(Channel.firebaseCollectionName).document(channel.identifier).collection(Message.firebaseCollectionName)
+        return db.collection(Channel.firebaseCollectionName)
+            .document(channel.identifier)
+            .collection(Message.firebaseCollectionName)
+            .order(by: "created", descending: false)
     }()
 
     // MARK: - Variables
     var currentUser: User!
     var channel: Channel!
-    var messages: [Message]?
     
     // MARK: - UI Variables
     private lazy var tableView: UITableView = {
@@ -29,6 +32,7 @@ class ConversationViewController: UIViewController {
         tableView.dataSource = self
         tableView.separatorStyle = .none
         tableView.rowHeight = UITableView.automaticDimension
+        tableView.allowsSelection = false
         return tableView
     }()
 
@@ -48,7 +52,7 @@ class ConversationViewController: UIViewController {
     private func setupView() {
         initTableView()
         initNavigation()
-        addDatabaseListener()
+        messageDataSource = FirebaseDataSource<Message>(for: tableView, with: messagesQuery)
     }
     
     private func updateView() {
@@ -65,31 +69,20 @@ class ConversationViewController: UIViewController {
 }
 
 extension ConversationViewController: UITableViewDataSource {
-    func addDatabaseListener() {
-        messagesReference.addSnapshotListener { [weak self] (docsSnapshot, _) in
-            guard let documents = docsSnapshot?.documents else { return }
-            self?.messages = documents.compactMap({ queryDocumentSnapshot -> Message? in
-                let data = queryDocumentSnapshot.data()
-                guard let content = data["content"] as? String,
-                      let created = data["created"] as? Timestamp,
-                      let senderId = data["senderId"] as? String,
-                      let senderName = data["senderName"] as? String
-                else { return nil }
-                return Message(content: content, created: created.dateValue(), senderId: senderId, senderName: senderName)
-            })
-            self?.tableView.reloadData()
-        }
-    }
-        
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { messages?.count ?? 0 }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { messageDataSource.elements.count }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: messageCellIdentifier, for: indexPath)
-        
-        if let messageCell = cell as? MessageTableViewCell, let message = messages?[indexPath.row] {
-            messageCell.configure(with: message.cellModel(for: currentUser))
+        guard let messageCell = cell as? MessageTableViewCell else {
+            return cell
         }
         
-        return cell
+        if let message = messageDataSource.elements[indexPath.row] {
+            messageCell.configure(with: message.cellModel(for: currentUser))
+        } else {
+            messageCell.configure(with: MessageCellModel(text: "ERROR", date: Date(), income: false))
+        }
+        
+        return messageCell
     }        
 }
