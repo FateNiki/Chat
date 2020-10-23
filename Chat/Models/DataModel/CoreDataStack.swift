354a2ae2c3f8970ce431af1092e266ff34d45642
Aleksey Nikitin <fateniki@gmail.com>
2020-10-23 18:36:38 +0400

    Создал класс CoreDataStack

---
+++ Chat/Models/DataModel/CoreDataStack.swift
@@ -0,0 +1,73 @@
+//
+//  CoreDataStack.swift
+//  Chat
+//
+//  Created by Алексей Никитин on 23.10.2020.
+//  Copyright © 2020 Алексей Никитин. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    private let dataModelName = "Chat"
+    private let dataModelExtension = "momd"
+    private var storeUrl: URL = {
+        guard let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last else {
+            fatalError("document path not found")
+        }
+        return documentsUrl.appendingPathComponent("Chat.sqlite")
+    }()
+    
+    // MARK: - Init Stack
+    private(set) lazy var managedObjectModel: NSManagedObjectModel = {
+        guard let moduleUrl = Bundle.main.url(forResource: dataModelName, withExtension: dataModelExtension) else {
+            fatalError("module not found")
+        }
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: moduleUrl) else {
+            fatalError("managedObjectModel could not be created")
+        }
+        return managedObjectModel
+    }()
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+        
+        do {
+            // TODO отдельная очередь
+            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType,
+                                               configurationName: nil,
+                                               at: storeUrl,
+                                               options: nil)
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+        
+        return coordinator
+    }()
+    
+    // MARK: - Contexts
+    private lazy var writterContext: NSManagedObjectContext = {
+        let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        context.persistentStoreCoordinator = persistentStoreCoordinator
+        context.mergePolicy = NSOverwriteMergePolicy
+        return context
+    }()
+    
+    private(set) lazy var mainContext: NSManagedObjectContext = {
+        let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        context.parent = writterContext
+        context.automaticallyMergesChangesFromParent = true
+        context.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
+        return context
+    }()
+    
+    private func saveContext() -> NSManagedObjectContext {
+        let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        context.parent = mainContext
+        context.automaticallyMergesChangesFromParent = true
+        context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        return context
+    }
+
+}
