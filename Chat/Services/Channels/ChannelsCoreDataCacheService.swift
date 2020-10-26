3da25f47da7db802fb574449eb2b42db75f626ca
Aleksey Nikitin <fateniki@gmail.com>
2020-10-26 20:32:32 +0400

    Обновление кеша в при обращении к базе данных

--- Chat/Services/Channels/ChannelsCoreDataCacheService.swift
+++ Chat/Services/Channels/ChannelsCoreDataCacheService.swift
@@ -14,12 +14,44 @@ class ChannelsCoreDataCacheService: ChannelsCacheService {
     
     init(changeCallback: @escaping ([Channel]) -> Void) {
         self.cacheDidChange = changeCallback
+        initObserver()
+    }
+    
+    private func initObserver() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(observeNotificatino(_:)),
+            name: .NSManagedObjectContextDidSave,
+            object: nil)
+    }
+    
+    @objc private func observeNotificatino(_ notification: Notification) {
+        guard let context = notification.object as? NSManagedObjectContext else { return }
+        guard context.parent == nil else { return }
+        
+        if let insertedObjects = notification.userInfo?[NSInsertedObjectsKey] as? Set<NSManagedObject>, !insertedObjects.isEmpty {
+            print("\tinsertedObjects", insertedObjects.count)
+            getChannels(self.cacheDidChange)
+            return
+        }
+        if let updatedObjects = notification.userInfo?[NSUpdatedObjectsKey] as? Set<NSManagedObject>, !updatedObjects.isEmpty {
+            print("\tupdatedObjects", updatedObjects.count)
+            getChannels(self.cacheDidChange)
+            return
+        }
+        if let deletedObjects = notification.userInfo?[NSDeletedObjectsKey] as? Set<NSManagedObject>, !deletedObjects.isEmpty {
+            print("\tdeletedObjects", deletedObjects.count)
+            getChannels(self.cacheDidChange)
+            return
+        }
     }
     
     func getChannels(_ completion: @escaping ([Channel]) -> Void) {
         print("RETURN cache channels")
         do {
-            guard let result = try coreDataStack.mainContext.fetch(ChannelDB.fetchRequest()) as? [ChannelDB] else { return }
+            let request: NSFetchRequest<ChannelDB> = ChannelDB.fetchRequest()
+            request.sortDescriptors = [NSSortDescriptor(key: "lastActivity", ascending: false)]
+            let result = try coreDataStack.mainContext.fetch(request)
             completion(result.compactMap { Channel(from: $0)})
         } catch {
             completion([Channel]())
@@ -28,15 +60,14 @@ class ChannelsCoreDataCacheService: ChannelsCacheService {
     
     func syncChannels(_ channels: [Channel]) {
         coreDataStack.performSave { saveContext in
-            let result = channels.compactMap { channel in
-                ChannelDB(identifier: channel.identifier,
+            print("SYNC START")                        
+            channels.forEach { channel in
+                _ = ChannelDB(identifier: channel.identifier,
                           name: channel.name,
                           lastMessage: channel.lastMessage,
                           lastActivity: channel.lastActivity,
                           in: saveContext)
             }
-            print("SYNC \(result.count) channels")
-            self.cacheDidChange(channels)
         }
     }
 }
