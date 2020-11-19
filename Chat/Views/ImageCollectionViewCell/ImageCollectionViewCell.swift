878385516a2ef7299e7ac19f20bfdf466f689f8a
Aleksey Nikitin <fateniki@gmail.com>
2020-11-19 11:05:15 +0400

    конфигурация ячейки

--- Chat/Views/ImageCollectionViewCell/ImageCollectionViewCell.swift
+++ Chat/Views/ImageCollectionViewCell/ImageCollectionViewCell.swift
@@ -9,6 +9,30 @@
 import UIKit
 
 class ImageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var imagePreview: UIImageView!
+    
+    private var imageResult: ImageResult? {
+        didSet {
+            guard let imageResult = imageResult else {
+                imagePreview.image = nil
+                imagePreview.isHidden = true
+                activityIndicator.stopAnimating()
+                return
+            }
+            switch imageResult {
+            case let .success(image):
+                imagePreview.image = image
+                imagePreview.isHidden = false
+                activityIndicator.stopAnimating()
+            case let .loading(task):
+                imagePreview.image = nil
+                imagePreview.isHidden = true
+                activityIndicator.startAnimating()
+                task.resume()
+            }
+        }
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -16,5 +40,24 @@ class ImageCollectionViewCell: UICollectionViewCell {
         self.layer.borderColor = UIColor.lightGray.cgColor
         self.layer.cornerRadius = 8
     }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        if let imageResult = imageResult {
+            switch imageResult {
+            case let .loading(task):
+                task.suspend()
+            default:
+                break
+            }
+        }
+        imageResult = nil
+    }
+}
 
+extension ImageCollectionViewCell: ConfigurableView {
+    func configure(with model: ImageResult) {
+        self.imageResult = model
+    }
 }
