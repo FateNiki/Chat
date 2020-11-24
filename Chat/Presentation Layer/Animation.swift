7e4a0beae132bc3f4285fe59460b43db7bdd7102
Aleksey Nikitin <fateniki@gmail.com>
2020-11-24 11:12:00 +0400

    Добавил анимации эмблем

--- Chat/Presentation Layer/Animation.swift
+++ Chat/Presentation Layer/Animation.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class Animations {
+    static var emblemCell: CAEmitterCell = {
+        let cell = CAEmitterCell()
+        cell.contents = UIImage(named: "AppIcon")?.cgImage
+        cell.scale = 0.6
+        cell.scaleRange = 0.3
+        cell.lifetime = 5.0
+        cell.birthRate = 3
+        cell.velocity = -30
+        cell.velocityRange = -20
+        cell.yAcceleration = CGFloat.random(in: -30...30)
+        cell.xAcceleration = CGFloat.random(in: -30...30)
+        return cell
+    }()
+    
     public class func shake(_ view: UIView) {
         let keyTimes: [NSNumber] = [0, 0.25, 0.5, 0.75, 1]
         let keyPositionValues: [NSNumber] = [0, -5, 0, 5, 0]
@@ -62,5 +76,16 @@ class Animations {
         group.duration = 0.3
         
         view.layer.add(group, forKey: "stab")
+    }
+    
+    public class func showEmblem(into view: UIView, at position: CGPoint) {
+        let emblemLayer = CAEmitterLayer()
+        emblemLayer.emitterPosition = position
+        emblemLayer.emitterSize = CGSize(width: 10, height: 10)
+        emblemLayer.emitterShape = .circle
+        emblemLayer.beginTime = CACurrentMediaTime()
+        emblemLayer.timeOffset = CFTimeInterval(1)
+        emblemLayer.emitterCells = [emblemCell]
+        view.layer.addSublayer(emblemLayer)
     }
 }
